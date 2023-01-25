293410028b462246e42b5f746540249ba5e9e340
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@redge.com>
2023-01-25 06:29:27 +0100

    Add countdown timer

---
+++ src/views/timer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default () => {
+
+  return (
+      <View>
+        <Text>Test timer</Text>
+      </View>
+    )
+};
